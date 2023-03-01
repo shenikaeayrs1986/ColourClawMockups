79d4555f6c49c40f03af2c1642cffb6f72847c61
shenikaeayrs1986 <125155162+shenikaeayrs1986@users.noreply.github.com>
2023-03-01 12:58:39 -0500

    Add files via upload

---
+++ Basic Output with Variables- Java.java
@@ -0,0 +1,20 @@
+import java.util.Scanner; 
+
+public class OutputWithVars {
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      int userNum;
+      int userNum2;
+      userNum = scnr.nextInt();
+      userNum2 = scnr.nextInt();
+      System.out.println("Enter integer: ");
+      System.out.println("You entered: " + userNum);
+      System.out.println(userNum + " squared is " + userNum * userNum);
+      System.out.println("And " + userNum + " cubed is " + userNum * userNum * userNum + "!!");
+      System.out.println("Enter another integer: ");
+      System.out.println(userNum + " + " + userNum2 + " is " + (userNum + userNum2));
+      System.out.println(userNum + " * " + userNum2 + " is " + userNum*userNum2);
+      
+      return;
+   }
+}
